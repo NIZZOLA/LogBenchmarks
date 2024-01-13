c34adb041dfa7423ff6b4e430ee043dafe46a024
Marcio Rogerio Nizzola <marcio@nizzola.com.br>
2024-01-13 16:16:25 -0300

    Add project files.

---
+++ LogBenchmarks/LogBench.cs
@@ -0,0 +1,62 @@
+﻿using BenchmarkDotNet.Attributes;
+using Serilog;
+using Serilog.Data;
+
+namespace LogBenchmarks;
+
+[MemoryDiagnoser]
+public class LogBench
+{
+    private ILogger logger;
+    private int number = 10;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        logger = new LoggerConfiguration()
+            .MinimumLevel.Error()
+          .WriteTo.Console()
+          .CreateLogger();
+    }
+
+    [Benchmark]
+    public void BenchmarkPlainTextLog()
+    {
+        
+        logger.Information("Teste de log");
+    }
+
+    [Benchmark]
+    public void BenchmarkStringInterplolationLog()
+    {
+
+        logger.Information($"Teste de log {number}");
+    }
+
+    [Benchmark]
+    public void BenchmarkStringConcatenatedLog()
+    {
+        logger.Information($"Teste de log "+ number);
+    }
+
+    [Benchmark]
+    public void BenchmarkPlainTextLogDisabled()
+    {
+        if (logger.IsEnabled(Serilog.Events.LogEventLevel.Information))
+            logger.Information("Teste de log");
+    }
+
+    [Benchmark]
+    public void BenchmarkStringInterplolationLogDisabled()
+    {
+        if (logger.IsEnabled(Serilog.Events.LogEventLevel.Information))
+            logger.Information($"Teste de log {number}");
+    }
+
+    [Benchmark]
+    public void BenchmarkStringConcatenatedLogDisabled()
+    {
+        if (logger.IsEnabled(Serilog.Events.LogEventLevel.Information))
+            logger.Information($"Teste de log " + number);
+    }
+}
